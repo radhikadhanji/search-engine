790ea155d8027b2810fb09736ebfb7ff9ba25ead
radhikadhanji <radhikadhanji0517@gmail.com>
2025-06-22 15:17:56 +1200

    Switch to processpoolexecutor

--- crawler.py
+++ crawler.py
@@ -1,6 +1,5 @@
-import requests as req
 import time
-import concurrent.futures as futures
+from concurrent.futures import ProcessPoolExecutor
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
@@ -19,12 +18,15 @@ def crawl(url, visited = set()):
     next = soup.find('li', class_ = 'next')
     if next:
         next_url = next.find('a')['href']
+        print(next_url)
         crawl(url + next_url, visited)
     driver.quit()
 
-urls = ['http://quotes.toscrape.com/js', 'https://www.octoparse.com/blog/how-to-build-a-web-crawler-from-scratch-a-guide-for-beginners']
-with futures.ThreadPoolExecutor() as executor:
-    executor.map(crawl, urls)
+if __name__ == '__main__':
+    urls = ['http://quotes.toscrape.com/js', 'https://www.octoparse.com/blog/how-to-build-a-web-crawler-from-scratch-a-guide-for-beginners']
+    with ProcessPoolExecutor() as executor:
+        #Multithreading with ProcessPoolExecutor
+        executor.map(crawl, urls)
 
 
 

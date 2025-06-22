cbcff6f81324a4a82f9a990fa419024cd78c3a74
radhikadhanji <radhikadhanji0517@gmail.com>
2025-06-22 15:00:28 +1200

    Use selenium web driver for scraping JS

--- crawler.py
+++ crawler.py
@@ -1,21 +1,25 @@
 import requests as req
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
-def crawl(url, visited = set()):
+def crawl(url, driver, visited = set()):
     if url in visited:
         #return if url is already visited
         return
     visited.add(url)
-    response = req.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    #Use safari web driver to detect JS
+    driver.get(url)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
     next = soup.find('li', class_ = 'next')
     if next:
         next_url = next.find('a')['href']
         print(next_url)
-        crawl(url + next_url, visited)
+        crawl(url + next_url, driver, visited)
 
-url = 'http://quotes.toscrape.com'
-crawl(url)
+url = 'http://quotes.toscrape.com/js'
+driver = webdriver.Safari()
+crawl(url, driver)
+driver.quit()
 
 
 

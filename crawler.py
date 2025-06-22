0a0670f4bba891ee5750edb14fb157f3a057e9bb
radhikadhanji <radhikadhanji0517@gmail.com>
2025-06-22 21:23:18 +1200

    WIP update crawler to save images

--- crawler.py
+++ crawler.py
@@ -3,8 +3,9 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from urllib.parse import urljoin
+import sqlite3
 
-def crawl(url, driver, visited = set()):
+def crawl(url, driver, connection, c, visited = set()):
     if requests.get(url).status_code != 200:
         print("Error: cannot fetch this url")
         return
@@ -19,9 +20,11 @@ def crawl(url, driver, visited = set()):
     time.sleep(2)
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     #Find all links and crawl the connected pages
-    next = soup.find('li', class_='next')
     images = soup.find_all('img')
     links = soup.find_all('a', href=True)
+    #Add to db (in order: outlinks, backlinks, images)
+    c.execute('''INSERT INTO webpages VALUES(?,?,?)''', (links, links, images))
+    connection.commit()
     for img in images:
         #get url of all images 
         image_url = urljoin(url, img.get('src'))
@@ -30,12 +33,15 @@ def crawl(url, driver, visited = set()):
         next_url = link['href']
         #If next url is a wikipedia article and not a subarticle, crawl
         if next_url.startswith('/wiki/') and ':' not in next_url:
-            crawl(urljoin(url, next_url), driver, visited)
+            crawl(urljoin(url, next_url), driver, connection, c, visited)
 
 if __name__ == '__main__':
     url = 'https://en.wikipedia.org/wiki/Cat'
     webdrive = webdriver.Safari()
-    crawl(url, webdrive)
+    connection = sqlite3.connect('searchengine.db')
+    c = connection.cursor()
+    crawl(url, webdrive, connection, c)
+    connection.close()
     webdrive.quit()
 
 

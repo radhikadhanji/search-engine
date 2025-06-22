4b4b634f26e22be7a5f11d865ae81ba149ee0863
radhikadhanji <radhikadhanji0517@gmail.com>
2025-06-22 21:13:41 +1200

    Add example data to database

--- datahandler.py
+++ datahandler.py
@@ -4,3 +4,13 @@ connection = sqlite3.connect('searchengine.db')
 c = connection.cursor()
 #c.execute('''CREATE TABLE webpages(outlinks TEXT, backlinks TEXT, images TEXT)''')
 
+outlinks = 'http://quotes.toscrape.com'
+backlinks = 'http://books.toscrape.com/?'
+images = ''
+
+#c.execute('''INSERT INTO webpages VALUES(?, ?, ?)''', (outlinks, backlinks, images))
+#connection.commit()
+
+c.execute('''SELECT * FROM webpages''')
+results = c.fetchall()
+print(results)

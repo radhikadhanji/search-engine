500492257b490a7a04bf19c3c3d8b1113399ca4a
radhikadhanji <radhikadhanji0517@gmail.com>
2025-06-22 14:38:26 +1200

    Update crawler.py
    Parse HTML

--- crawler.py
+++ crawler.py
@@ -1,5 +1,5 @@
 import requests as req
-#from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 
 url = 'http://quotes.toscrape.com'
 response = req.get(url)
@@ -7,4 +7,8 @@ if response.status_code == 200:
     print("The page was fetched successfully!")
 else:
     print("The page could not be fetched.")
+
+soup = BeautifulSoup(response.text, 'html.parser')
+#Find all links in the url
+links = soup.find_all('a')
 

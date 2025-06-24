bb4ab75feabbe4e1a14254ab799741792d739b26
radhikadhanji <radhikadhanji0517@gmail.com>
2025-06-24 14:48:32 +1200

    Changed database architecture (separate webpages, backlinks and outlinks)

--- datahandler.py
+++ datahandler.py
@@ -2,15 +2,7 @@ import sqlite3
 
 connection = sqlite3.connect('searchengine.db')
 c = connection.cursor()
-#c.execute('''CREATE TABLE webpages(outlinks TEXT, backlinks TEXT, images TEXT)''')
-
-outlinks = 'http://quotes.toscrape.com'
-backlinks = 'http://books.toscrape.com/?'
-images = ''
-
-#c.execute('''INSERT INTO webpages VALUES(?, ?, ?)''', (outlinks, backlinks, images))
-#connection.commit()
-
-c.execute('''SELECT * FROM webpages''')
-results = c.fetchall()
-print(results)
+c.execute('''CREATE TABLE IF NOT EXISTS webpages(url TEXT, images TEXT)''')
+c.execute('''CREATE TABLE IF NOT EXISTS backlinks(source TEXT, backlink TEXT)''')
+c.execute('''CREATE TABLE IF NOT EXISTS outlinks(source TEXT, outlink TEXT)''')
+connection.commit()

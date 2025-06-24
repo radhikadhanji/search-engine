beea78b22de8c2f6d11e8853c4e86875a7ddae6f
radhikadhanji <radhikadhanji0517@gmail.com>
2025-06-24 15:11:54 +1200

    Prevent duplicate values with uniqueness constraint

--- datahandler.py
+++ datahandler.py
@@ -2,7 +2,7 @@ import sqlite3
 
 connection = sqlite3.connect('searchengine.db')
 c = connection.cursor()
-c.execute('''CREATE TABLE IF NOT EXISTS webpages(url TEXT, images TEXT)''')
-c.execute('''CREATE TABLE IF NOT EXISTS backlinks(source TEXT, backlink TEXT)''')
-c.execute('''CREATE TABLE IF NOT EXISTS outlinks(source TEXT, outlink TEXT)''')
+c.execute('''CREATE TABLE IF NOT EXISTS webpages(url TEXT, images TEXT, UNIQUE(url, images))''')
+c.execute('''CREATE TABLE IF NOT EXISTS backlinks(source TEXT, backlink TEXT, UNIQUE(source, backlink))''')
+c.execute('''CREATE TABLE IF NOT EXISTS outlinks(source TEXT, outlink TEXT, UNIQUE(source, outlink))''')
 connection.commit()
